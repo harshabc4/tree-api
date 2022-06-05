7260225b1e026f36078cb946ca8b63774edba6f9
Brian Harsha <harshabc4@gmail.com>
2022-06-05 08:49:17 -0400

    working tree api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000
+
+const woodSpecies = {
+    'property1': {
+        'dataset1' : 12345,
+        'dataset2' : 'asdf',
+        'dataset3' : 'asdf'
+    },
+    'property2' : {
+        'dataset1' : 12345,
+        'dataset2' : 'asdf',
+        'dataset3' : 'asdf'
+    },
+    'unknown' : {
+        'dataset1' : 12345,
+        'dataset2' : '?',
+        'dataset3' : 'asdf'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const treeName = request.params.name.toLowerCase()
+    // console.log(woodSpecies[treename].dataset1)
+    if( woodSpecies[treeName] ){
+        response.json(woodSpecies[treeName])
+    }else{
+        response.json(woodSpecies['unknown'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}!`)
+})
